ff72ae2535ed4966d78730b7bc5644dae570f1de
Gabriel <gabpavao@outlook.com>
2023-04-07 18:43:25 -0300

    feat: create sendEmailRequest function

--- src/contexts/DatabaseContext.tsx
+++ src/contexts/DatabaseContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { api } from '../services/api';
-import { iDatabaseProviderValues, iProject, iProviderChildrenProps } from './interfaces';
+import { iDatabaseProviderValues, iEmailRequestData, iProject, iProviderChildrenProps } from './interfaces';
 
 export const DatabaseContext: React.Context<iDatabaseProviderValues> = createContext({} as iDatabaseProviderValues)
 
@@ -16,6 +16,14 @@ export function DatabaseProvider ({ children }: iProviderChildrenProps): JSX.Ele
 		}
 	}
 
+	async function sendEmailRequest (emailData: iEmailRequestData): Promise<void> {
+		try {
+			const response = await api.post('/email', emailData)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
 	useEffect(() => {
 		async function handleGetProjects (): Promise<void> {
 			await getAllProjects()
@@ -25,7 +33,7 @@ export function DatabaseProvider ({ children }: iProviderChildrenProps): JSX.Ele
 	}, [])
 	
 	return (
-		<DatabaseContext.Provider value={{ projects, setProjects }}>
+		<DatabaseContext.Provider value={{ projects, setProjects, sendEmailRequest }}>
 			{children}
 		</DatabaseContext.Provider>
 	)
